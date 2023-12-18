f4476ffe85623da2b324a6d1c7678ddb34ea23ff
Mayra016 <mayrazucaro@gmail.com>
2023-12-18 18:21:18 -0300

    fix bug connection with database failure

--- ProyectoCrud/src/main/java/com/test/teste/Entities/UserEntity.java
+++ ProyectoCrud/src/main/java/com/test/teste/Entities/UserEntity.java
@@ -1,53 +1,47 @@
 package com.test.teste.Entities;
-import org.springframework.data.annotation.Id;
 
-
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Column;
-
-
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "user")
 public class UserEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String name;
+
     @Column(name = "telephone")
     private String phoneNumber;
-    
 
-    public UserEntity(long id, String name, String phoneNumber) {
-        this.id = id;
+    // Empty constructor required by JPA
+    public UserEntity() {
+    }
+
+    public UserEntity(String name, String phoneNumber) {
         this.name = name;
         this.phoneNumber = phoneNumber;
     }
-    
-    public long getId() {
+
+    public Long getId() {
         return id;
     }
-    
-    public void setId(long newId) {
-        this.id = newId;
-    }
-    
+
+    // No setId method to prevent setting the ID manually
+
     public String getName() {
         return this.name;
     }
-    
+
     public void setName(String newName) {
         this.name = newName;
     }
-    
+
     public String getPhoneNumber() {
         return this.phoneNumber;
     }
-    
+
     public void setPhoneNumber(String newPhoneNumber) {
         this.phoneNumber = newPhoneNumber;
     }
--- ProyectoCrud/src/main/java/com/test/teste/TesteApplication.java
+++ ProyectoCrud/src/main/java/com/test/teste/TesteApplication.java
@@ -3,10 +3,12 @@ package com.test.teste;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.test.teste.Repositories", entityManagerFactoryRef = "entityManagerFactory")
+@ComponentScan(basePackages = "com.test.teste")
 public class TesteApplication {
 
 	public static void main(String[] args) {
